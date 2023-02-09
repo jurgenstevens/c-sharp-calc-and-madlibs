ea6a791a1953923787f5928e1d89eb39c837f9fc
jurgenstevens <jurgen.stevens@yahoo.com>
2023-02-08 22:04:14 -0600

    Complete calculator and madlibs game functionality.

--- CalcAndMadLibs.cs
+++ CalcAndMadLibs.cs
@@ -7,13 +7,30 @@ namespace CalcAndMadLibs
 {
     class CalcAndMadLibs
     {
-
-
         static void Main(string[] args)
         {
-            Console.WriteLine("Roses are {color}");
-            Console.WriteLine("{plural noun} are blue");
-            Console.WriteLine("I love {celebrity}");
+            // using the data type: double will allow you to use ints and decimals
+            //Console.Write("Enter a number: ");
+            //double num1 = Convert.ToDouble(Console.ReadLine());
+
+            //Console.Write("Enter another number: ");
+            //double num2 = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine(num1 + num2);
+            //Console.ReadLine();
+
+            string color, pluralNoun, celebrity;
+
+            Console.Write("Enter a color: ");
+            color = Console.ReadLine();
+            Console.Write("Enter a plural noun: ");
+            pluralNoun = Console.ReadLine();
+            Console.Write("Enter a celebrity: ");
+            celebrity = Console.ReadLine();
+
+            Console.WriteLine("Roses are " + color);
+            Console.WriteLine(pluralNoun + " are blue");
+            Console.WriteLine("I love " + celebrity);
 
             Console.ReadLine();
         }
