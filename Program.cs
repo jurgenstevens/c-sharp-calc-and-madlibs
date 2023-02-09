1dabfd058dc11282ac57aa4d39aa030935332a29
jurgenstevens <jurgen.stevens@yahoo.com>
2023-02-08 21:26:06 -0600

    Complete string to int conversion.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            int num = Convert.ToInt32("45");
+            Console.WriteLine(num + 6);
+
+            Console.ReadLine();
+        }
+    }
+}
 
