80f472453a44cdd3e4bed61c3908369e0c2910b7
jurgenstevens <jurgen.stevens@yahoo.com>
2023-02-09 14:54:01 -0600

    Complete calculator with operator.

--- CalcAndMadLibs.cs
+++ CalcAndMadLibs.cs
@@ -9,6 +9,7 @@ namespace CalcAndMadLibs
     {
         static void Main(string[] args)
         {
+            // FIRST CALCULATOR
             // using the data type: double will allow you to use ints and decimals
             //Console.Write("Enter a number: ");
             //double num1 = Convert.ToDouble(Console.ReadLine());
@@ -31,6 +32,37 @@ namespace CalcAndMadLibs
             Console.WriteLine("Roses are " + color);
             Console.WriteLine(pluralNoun + " are blue");
             Console.WriteLine("I love " + celebrity);
+
+            // BETTER CALCULATOR WITH OPERATOR
+            Console.Write("Enter a number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter an operator: ");
+            string op = Console.ReadLine();
+
+            Console.Write("Enter another number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            if (op == "+")
+            {
+                Console.WriteLine(num1 + num2);
+            }
+            else if (op == "-")
+            {
+                Console.WriteLine(num1 - num2);
+            }
+            else if (op == "*")
+            {
+                Console.WriteLine(num1 * num2);
+            }
+            else if (op == "/")
+            {
+                Console.WriteLine(num1 / num2);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Operator.");
+            }
 
             Console.ReadLine();
         }
